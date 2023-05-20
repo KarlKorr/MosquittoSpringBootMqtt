4f8c18b73bc07604048ff8ab1c41154ec447c2f6
karlkorr <carlcorr97@gmail.com>
2023-05-20 19:15:53 +0200

    Questa versione è predisposta ad accettare le coordinate da un clinet remoto. E' stata aggiunta una classe le coordinate da un client remoto. Per adempire a questo scopo, è stata aggiunta una classe Coordinate. Inoltre, nel controller è stato modificato il metodo sendMessage per accettare e le coordinate.

--- demo(2)/demo/src/main/java/com/example/demo/Controller/MqttController.java
+++ demo(2)/demo/src/main/java/com/example/demo/Controller/MqttController.java
@@ -1,12 +1,15 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Coordinate;
 import com.example.demo.MqttGateway;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,19 +18,47 @@ public class MqttController {
     @Autowired
     MqttGateway mqttGateway;
 
+
     @PostMapping("/sendMessage")
-    public ResponseEntity<?> publish(@RequestBody String mqttMessage){
+    public ResponseEntity<String> sendMessage(@RequestParam("topic") String topic, Coordinate message) {
 
-        try {
+        System.out.println("In questo momento, le coordinate sono: " + message);
 
-            JsonObject convertObject = new Gson().fromJson(mqttMessage, JsonObject.class);
-            mqttGateway.senToMqtt(convertObject.get("message").toString(), convertObject.get("topic").toString());
-            return ResponseEntity.ok("Success");
-        }
-        catch(Exception ex){
-            ex.printStackTrace();
-            return ResponseEntity.ok("fail");
-        }
+        mqttGateway.senToMqtt(String.valueOf(message), topic);
+        return ResponseEntity.ok("Success");
+
     }
+
+
+//    @PostMapping("/sendMessage")
+//    public ResponseEntity<?> publish(@RequestBody String mqttMessage){
+//
+//        try {
+//
+//            JsonObject convertObject = new Gson().fromJson(mqttMessage, JsonObject.class);
+//            mqttGateway.senToMqtt(convertObject.get("message").toString(), convertObject.get("topic").toString());
+//            return ResponseEntity.ok("Success");
+//        }
+//        catch(Exception ex){
+//            ex.printStackTrace();
+//            return ResponseEntity.ok("fail");
+//        }
+//    }
+
+//    @PostMapping("/sendMessage/{mqttMessage}")
+//    public ResponseEntity<?> publish(@RequestBody JsonObject mqttMessage) {
+//        try {
+//            String message = mqttMessage.get("message").getAsString();
+//            String topic = mqttMessage.get("topic").getAsString();
+//
+//            mqttGateway.senToMqtt(message, topic);
+//            return ResponseEntity.ok("Success");
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//            return ResponseEntity.ok("fail");
+//        }
+//    }
+
+
 
 }
---
+++ demo(2)/demo/src/main/java/com/example/demo/Coordinate.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+public class Coordinate {
+
+    private double latitude;
+    private double longitude;
+
+    public Coordinate(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Coordinate(){}
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "latitude=" + latitude +
+                ", longitude=" + longitude;
+    }
+}
