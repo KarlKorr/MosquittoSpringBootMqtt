271be059605c2259421a255a331fe12869bbe17d
karlkorr <carlcorr97@gmail.com>
2023-08-08 16:28:01 +0200

    payload aggiornato

--- demo(2)/demo/src/main/java/com/example/demo/MqttBeans.java
+++ demo(2)/demo/src/main/java/com/example/demo/MqttBeans.java
@@ -85,7 +85,7 @@ public class MqttBeans {
                 System.out.println(message.getPayload());
                 System.out.println("Il messaggio ricevuto è uguale a: " + message);
                 System.out.println("Il messaggio stringato è uguale a: " + message.toString());
-                Coordinate coordinate = new Gson().fromJson(message.toString(), Coordinate.class);
+                Coordinate coordinate = new Gson().fromJson(message.getPayload().toString(), Coordinate.class);
                 System.out.println("Il messaggio deserializzato è uguale a: " + coordinate);
                 mqttService.sendMessage(coordinate, coordinate.getTopic());
 
